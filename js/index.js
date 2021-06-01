ae88b518ccc4cd4b526b386ee42aba0793c06a4b
Denis Kozarenko <denis_kozarenko@ukr.net>
2021-06-01 17:25:09 +0300

    alert appear

--- js/index.js
+++ js/index.js
@@ -9,6 +9,18 @@ class StartButton {
   }
 }
 
+class GameScore {
+  constructor(score, counter) {
+    this.score = score;
+    this.counter = counter;
+    this.score.textContent = counter;
+  }
+  addOnePoint() {
+    this.counter += 1;
+    this.score.textContent = this.counter;
+  }
+}
+
 class TableCell {
   constructor(tableCell) {
     this.tableCell = tableCell;
@@ -20,16 +32,22 @@ class TableCell {
     for (const key in this) {
       const currentCell = this[key][this.getRndInteger(0, 99)];
       if (currentCell.className === "blue") {
-        return;
+        return currentCell;
       } else {
         currentCell.classList.add("blue");
+        return currentCell;
       }
     }
   }
   listenClick() {
     for (const key in this.tableCell) {
       if (this.tableCell[key].tagName === "TD") {
-        this.tableCell[key].addEventListener("click", () => {});
+        this.tableCell[key].addEventListener("click", () => {
+          if (this.tableCell[key].className === "blue") {
+            this.tableCell[key].classList.remove("blue");
+            this.tableCell[key].classList.toggle("green");
+          }
+        });
       }
     }
   }
@@ -41,17 +59,63 @@ class DifficultyLevel {
   }
 }
 
-const difficultyLevel = new DifficultyLevel(1500);
+class GameAlert {
+  constructor(alert, alertTitle, alertTitleMsg, alertMsg, alertText) {
+    this.alert = alert;
+    // this.alertTitle = alertTitle;
+    // this.alertTitle.textContent = alertTitleMsg;
+    // this.alertMsg = alertMsg;
+    // this.alertMsg.textContent = alertText;
+  }
+  showAlert() {
+    this.alert.classList.add("display_block");
+  }
+}
+
+const userScore = new GameScore(document.querySelector(".user_number"), 0);
+
+const computerScore = new GameScore(
+  document.querySelector(".computer_number"),
+  0
+);
+
+const difficultyLevel = new DifficultyLevel(10);
 
 const startButton = new StartButton(document.querySelector(".button"));
 
-startButton.buttonHandler();
-
 const tableCell = new TableCell(document.querySelectorAll("td"));
 
+startButton.buttonHandler();
+
 function startGame() {
+  if (this.className === "button active") return;
+  this.classList.add("active");
   tableCell.listenClick();
-  setInterval(() => {
-    tableCell.activateRndCell();
+  let timer = setTimeout(function tick() {
+    if (computerScore.counter >= 50 || userScore.counter >= 50) {
+      clearTimeout(timer);
+      const gameAlert = new GameAlert(document.querySelector(".game_alert"));
+      console.log(gameAlert);
+      gameAlert.showAlert();
+      return;
+    }
+    let activeCell = tableCell.activateRndCell();
+    activeCell.addEventListener("click", () => {
+      console.log(activeCell.classList, activeCell.className);
+      if (activeCell.className === "green") {
+        activeCell.classList.add("clicked");
+        userScore.addOnePoint();
+      }
+    });
+    let redTimer = setTimeout(function redTick() {
+      if (activeCell.className === "blue") {
+        activeCell.classList.remove("blue");
+        activeCell.classList.add("red");
+        computerScore.addOnePoint();
+      }
+      redTimer = setTimeout(redTick, difficultyLevel.difficulty);
+    }, difficultyLevel.difficulty);
+
+    timer = setTimeout(tick, difficultyLevel.difficulty);
   }, difficultyLevel.difficulty);
 }
