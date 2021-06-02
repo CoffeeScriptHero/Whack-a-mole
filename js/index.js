ba6e8a90bab6688f7bc14db161c07cf83ab33524
Denis Kozarenko <denis_kozarenko@ukr.net>
2021-06-02 16:25:40 +0300

    whack-a-mole done

--- js/index.js
+++ js/index.js
@@ -7,14 +7,16 @@ class Button {
   buttonHandler() {
     this.button.addEventListener("click", startGame);
   }
+  beginGame() {
+    startGame.bind(this.button)();
+  }
   changeDifficulty(time) {
     this.button.addEventListener("click", () => {
-      difficultyLevel.level = time;
       updateGame();
+      difficultyLevel.difficulty = time;
     });
   }
 }
-
 class GameScore {
   constructor(score, counter) {
     this.score = score;
@@ -33,7 +35,7 @@ class TableCell {
     this.tableCell = tableCell;
   }
   getRndCell(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * 100);
   }
   activateRndCell() {
     for (const key in this) {
@@ -68,37 +70,44 @@ class DifficultyLevel {
 
 class GameAlert {
   constructor(
-    alert,
+    alert = document.querySelector(".game_alert"),
     alertTitle = document.querySelector(".game_title"),
-    alertTitleMsg = 0,
-    alertMsg = document.querySelector(".game_message"),
-    alertText = 0
+    alertMsg = document.querySelector(".game_message")
   ) {
     this.alert = alert;
     this.alertTitle = alertTitle;
-    this.alertTitle.textContent = alertTitleMsg;
     this.alertMsg = alertMsg;
+  }
+  addTitleMsg(alertTitleMsg) {
+    this.alertTitle.textContent = alertTitleMsg;
+  }
+  addMsgText(alertText) {
     this.alertMsg.textContent = alertText;
   }
-  // addTitleMsg(titleMsg) {
-  //   this.alertTitle.textContent = alertTitleMsg;
-  // }
   showAlert() {
     this.alert.classList.add("display_block");
   }
+  hideAlert() {
+    this.alert.classList.remove("display_block");
+  }
 }
 
-class Timer {
-  constructor(delay) {
-    this.delay = delay;
-    this.remaining = this.delay;
-    this.timerId;
-    this.start;
+class MainTitle {
+  constructor(title) {
+    this.title = title;
   }
-  pause() {}
-  resume() {
-    start = Date.now();
-    timerId = setTimeout(() => {}, timeout);
+  changeColor() {
+    this.title.addEventListener("mousemove", () => {
+      const titleArr = this.title.textContent.split("");
+      const colorTitleArr = titleArr.map((element) => {
+        const span = document.createElement("span");
+        span.textContent = element;
+        span.style.color = getRandomColor();
+        return span;
+      });
+      this.title.textContent = "";
+      this.title.append(...colorTitleArr);
+    });
   }
 }
 
@@ -109,13 +118,13 @@ const computerScore = new GameScore(
   0
 );
 
-const difficultyLevel = new DifficultyLevel(5);
+const difficultyLevel = new DifficultyLevel(1000);
 
 const startButton = new Button(document.querySelector(".button"));
 
-const tableCell = new TableCell(document.querySelectorAll("td"));
-
 startButton.buttonHandler();
+
+const tableCell = new TableCell(document.querySelectorAll("td"));
 
 const easyBtn = new Button(document.querySelector(".easy_btn"));
 const mediumBtn = new Button(document.querySelector(".medium_btn"));
@@ -125,13 +134,23 @@ easyBtn.changeDifficulty(1500);
 mediumBtn.changeDifficulty(1000);
 hardBtn.changeDifficulty(500);
 
+const mainTitle = new MainTitle(document.querySelector(".main_title"));
+
+mainTitle.changeColor();
+
+const getRandomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
 const computerScoreAlert = (arg) => {
   clearTimeout(arg);
-  const gameAlert = new GameAlert(
-    document.querySelector(".game_alert"),
-    document.querySelector(".game_title"),
-    "Computer win",
-    document.querySelector(".game_message"),
+  const gameAlert = new GameAlert();
+  gameAlert.addTitleMsg("Computer win");
+  gameAlert.addMsgText(
     `Computer score: ${computerScore.counter}, your score: ${userScore.counter}`
   );
   gameAlert.showAlert();
@@ -139,11 +158,9 @@ const computerScoreAlert = (arg) => {
 
 const userScoreAlert = (arg) => {
   clearTimeout(arg);
-  const gameAlert = new GameAlert(
-    document.querySelector(".game_alert"),
-    document.querySelector(".game_title"),
-    "You win",
-    document.querySelector(".game_message"),
+  const gameAlert = new GameAlert();
+  gameAlert.addTitleMsg("You win");
+  gameAlert.addMsgText(
     `Computer score: ${computerScore.counter}, your score: ${userScore.counter}`
   );
   gameAlert.showAlert();
@@ -151,15 +168,28 @@ const userScoreAlert = (arg) => {
 
 let timer, redTimer, isFinished;
 
-// function updateGame() {
-//   tableCell.tableCell.forEach((element) => {
-//     element.className = "";
-//     const gameAlert = new GameAlert(document.querySelector(".game_alert"));
-//   });
-// }
+const scoreReset = () => {
+  userScore.score.textContent = 0;
+  computerScore.score.textContent = 0;
+  userScore.counter = 0;
+  computerScore.counter = 0;
+};
+
+const updateGame = () => {
+  tableCell.tableCell.forEach((element) => {
+    element.className = "";
+  });
+  const gameAlert = new GameAlert(document.querySelector(".game_alert"));
+  gameAlert.hideAlert();
+  scoreReset();
+  startButton.button.className = "button";
+  startButton.beginGame();
+};
+
 function startGame() {
-  if (!isFinished) startButton.button.textContent = "Start game";
+  if (!isFinished) startButton.button.textContent = "Start Game";
   if (this.className === "button game_started") {
+    clearTimeout(redTimer);
     clearTimeout(timer);
     this.classList.remove("game_started");
     startButton.button.textContent = "Resume game";
@@ -168,18 +198,8 @@ function startGame() {
   this.classList.add("game_started");
   tableCell.listenClick();
   timer = setTimeout(function tick() {
-    if (computerScore.counter >= 50) {
-      computerScoreAlert(timer);
-      isFinished = true;
-      return;
-    } else if (userScore.counter >= 50) {
-      userScoreAlert(timer);
-      isFinished = true;
-      return;
-    }
     let activeCell = tableCell.activateRndCell();
     activeCell.addEventListener("click", () => {
-      console.log(activeCell.classList, activeCell.className);
       if (activeCell.className === "green") {
         activeCell.classList.add("clicked");
         userScore.addOnePoint();
@@ -193,7 +213,15 @@ function startGame() {
       }
       redTimer = setTimeout(redTick, difficultyLevel.difficulty);
     }, difficultyLevel.difficulty);
-
+    if (computerScore.counter >= 50) {
+      computerScoreAlert(timer);
+      isFinished = true;
+      return;
+    } else if (userScore.counter >= 50) {
+      userScoreAlert(timer);
+      isFinished = true;
+      return;
+    }
     timer = setTimeout(tick, difficultyLevel.difficulty);
   }, difficultyLevel.difficulty);
 }
